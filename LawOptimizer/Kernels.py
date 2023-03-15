accd52053070b7d2263caedccbe7a720ae894077
Simona Capponi <you@example.com>
2023-03-15 15:42:12 +0000

    First commit

---
+++ LawOptimizer/Kernels.py
@@ -0,0 +1,163 @@
+# %%
+import numpy as np
+# import config
+from GPy.kern.src.rbf import RBF
+from GPy.kern import Kern
+from GPy.core.parameterization.param import  Param
+from paramz.transformations import Logexp
+from dscribe.kernels import REMatchKernel as Rematch
+
+# global  descriptors, targets
+
+# descriptors = config.descriptors
+# targets  = config.targets
+
+# def permute(descriptors,  targets, seed=None):
+    # if seed is not None:
+    #     np.random.seed(seed)
+    # idxs = np.random.permutation(range(len(targets)))
+    # descriptors = descriptors[idxs]
+    # targets = targets[idxs]
+    # return idxs, descriptors, targets
+
+
+# %%
+class CoulombKernel(Kern):
+
+    def __init__(self, input_dim, GPy_kern=None, domain=None, seed = None, active_dims=None, useGPU=False, *args, **options):
+        super().__init__(input_dim, active_dims, useGPU=useGPU, name="Coulomb", *args, **options)
+
+        self._name = "Coulomb"
+        self.kernel = GPy_kern
+        self.domain =domain
+        self.variance=self.kernel.variance
+        self.lengthscale=self.kernel.lengthscale
+        self.link_parameters(self.variance, self.lengthscale)
+        if options: 
+            self.options = options
+        else:
+            self.options = {'permutation':"eigenspectrum", 'flatten':False}
+
+    
+    def look_up(self, X):
+        ''' Utility  method to look-up for descriptor from index.
+            useful when the serch space is the space of the indexes
+            X : 2D array of indexes'''
+        idxs  = [np.where(xx>0)[0] for xx in list(X)]
+        idxs = np.vstack(idxs)
+        concs = X[X>0].reshape(-1,1)
+        D = [self.domain[int(j)] for j in idxs.flatten().tolist()]
+        D = np.vstack(D)
+        mols = np.hstack([concs, D])
+        return mols
+    
+
+    def K(self, X1, X2=None):
+
+        if X2 is None:
+            X2 = X1
+        descr_1 = self.look_up(X1)
+        descr_2 = self.look_up(X2)
+        return self.kernel.K(descr_1, X2=descr_2)
+
+    def Kdiag(self,X):
+        descr = self.look_up(X)
+        return self.kernel.Kdiag(descr)
+
+
+    def update_gradients_full(self, dL_dK, X1, X2=None):
+        X2=X1 if X2 is None else X2
+        descr_1 = self.look_up(X1)
+        descr_2 = self.look_up(X2)
+
+        self.kernel.update_gradients_full(dL_dK, descr_1, X2=descr_2)
+    
+    def to_dict(self):
+        # input_dict = {}
+        # GPy_kern_dict = self.GPy_kern.to_dict()
+        # input_dict['class'] = self._name 
+        # input_dict['kernel'] = GPy_kern_dict
+        # input_dict['variance'] = self.variance
+        # input_dict['lengthscale'] = self.lengthscale
+        # input_dict['options'] = self.options
+        pass
+
+
+    @staticmethod
+    def from_dict(input_dict):
+        # return super().from_dict(input_dict)
+        pass
+
+
+class GPyREMatchKern(Kern):
+    '''
+        domain is a dictionary {idx:molecule_dict}, where molecule_dict is an ase.Atoms dicctionary
+        from which it is pssible to build descriptors using DScribe.
+        domain is retrieved at the beginning of the experiment via read_config(). 
+
+    '''
+
+
+    def __init__(self, input_dim, domain=None, seed=None, active_dims=None, useGPU=False, 
+                 alpha=0.1, threshold=1e-6, metric="rbf", gamma=0.5, degree=3, 
+                 coef0=1, kernel_params=None, normalize_kernel=True):
+    
+        super(GPyREMatchKern, self).__init__(input_dim, active_dims, name = 'Rematch')
+
+        self.kernel=Rematch(alpha=alpha, threshold=threshold, metric=metric,
+                        gamma=gamma, degree=degree, coef0=coef0, kernel_params=kernel_params,
+                        normalize_kernel=normalize_kernel)
+
+        self._name = "Rematch"
+        self.metric = metric
+        self.domain = domain
+        self.gamma = Param("gamma", self.kernel.gamma, Logexp())
+        self.alpha = Param("alpha", self.kernel.alpha,Logexp())
+        self.degree = degree
+        self.coef0 = 1
+        self.kernel_params=kernel_params
+        self.normalize_kernel=normalize_kernel
+        self.threshold = threshold
+        self.link_parameters(self.gamma, self.alpha)
+
+    def look_up(self, X, n=1):
+        ''' Converts indexes into descriptors.
+            X: 2D array containing the indexes (int)
+            descriptors: the full set of descriptors, from which to 
+                         search by index.
+            returns D: list of descriptors'''
+        X_descr = X[:-n]
+        D = [self.domain[int(j)] for j in X_descr.flatten().tolist()]
+        return D
+
+    def K(self, X1, X2=None):
+        if X2 is None:
+            X2 = X1        
+        descr_1 = self.look_up(X1)
+        descr_2 = self.look_up(X2)
+        return self.kernel.create(descr_1, y=descr_2)
+
+    def Kdiag(self, X):
+
+        ''' '''
+        if self.kernel.normalize_kernel:
+            Kdiag = np.ones(len(X))
+        else:
+            Kdiag = np.zeros(len(X))
+            D =  self.look_up(X)
+            for j, d in enumerate(D):
+                Kdiag[j] = self.kernel.create([D[j]], y=[D[j]])
+        return Kdiag
+
+    def update_gradients_full(self, dL_dK, X1, X2=None):
+        pass
+
+    def to_dict(self):
+        # return super().to_dict()
+        pass
+        
+    @staticmethod
+    def from_dict(input_dict):
+        pass
+              
+
---
+++ LawOptimizer/LAW_2_ORDER.py
@@ -0,0 +1,242 @@
+# %%
+
+import numpy as np
+import copy
+from scipy.linalg import cho_factor, cho_solve
+from sklearn.preprocessing import normalize, minmax_scale
+from GPyOpt.models import GPModel
+from GPy.models import GPRegression
+from GPyOpt.core.task.space import Design_space
+import Kernels
+# %%
+class LAW_acq(object):
+    def __init__(self, model, weight_func, v_lim=10, normalize_values=False):
+        self.normalize = normalize_values
+        self.v_lim = v_lim
+        self.weight_func = weight_func
+        self.model = model
+
+    def compute_variance(self, X, x, noise=None):
+        """This function will be in the LAW_acq class:
+        var(x_j) = K(x_j, x_j) - K(x_j, X)(K(X,X) - sigma^2I)^(-1)K(X, x_j)
+        var(x_j) has shape len(x_j) X len((x_j))"""
+
+        kernel = self.model.model.kern
+        if noise is None:
+            noise = self.model.model.parameters[1].param_array
+        Kxx = kernel.K(x, x)
+        KxX = kernel.K(x, X)
+        KXX = kernel.K(X, X)
+        W = KXX + noise * np.eye(len(X))
+        W_inv = cho_solve(cho_factor(W), np.eye(len(W)))
+        temp = (KxX.dot(W_inv)).dot(KxX.T)
+        return Kxx - temp
+
+    def compute_value(self, AF, x, X=None):
+        print("AF.shape: ", AF.shape)
+        
+        var_size = self.v_lim
+        if X is None:
+            X = self.model.model.X
+        Gauss_noise = self.model.model.parameters[1].param_array
+        N = len(x) // var_size
+        p = len(x) % var_size
+        if N == 0:
+            var = np.diag(self.compute_variance(X, np.atleast_2d(x), noise=Gauss_noise))
+            var = var.reshape(-1, 1)
+        else:
+            var = np.zeros((len(x), 1))
+            for j in range(N):
+                cov = self.compute_variance(
+                    X, np.atleast_2d(x[j * var_size : (j + 1) * var_size]), noise=Gauss_noise
+                )
+                var[j * var_size : (j + 1) * var_size] = np.diag(cov).reshape(-1, 1)
+            if p !=0:
+                cov = self.compute_variance(
+                    X, np.atleast_2d(x[N * var_size : N * var_size + p]), noise=Gauss_noise
+                )
+                var[N * var_size : N * var_size + p] = np.diag(cov).reshape(-1, 1)
+        if self.normalize:
+            norm_var = minmax_scale(var)
+            norm_acq_value = minmax_scale(AF)
+            return norm_var * (self.weight_func(norm_acq_value)) ** 2
+        self.var = var
+        return var * (self.weight_func(AF)) ** 2
+
+
+
+
+
+class LAW_Evaluator(object):
+    def __init__(
+        self,
+        batch_size,
+        search_domain,
+        acquisition,
+        objective=None,
+        # n_jobs=1,
+        verbose=False,
+    ):
+        """acquisition_function: the GPyOpt acqusition function chosen
+        objective: the function to maximize in order to sample points for the batch"""
+
+        self.objective = objective
+        self.acquisition = acquisition
+        self.batch_size = batch_size
+        self.search_domain = search_domain
+        self.verbose = verbose
+
+    def compute_batch(self, verbose=True, X_testing=None):
+        L_obj_val = []
+
+        AF = self.acquisition._compute_acq(self.search_domain)
+        idx = np.argmax(AF)
+        X_af =np.atleast_2d(self.search_domain[idx])
+        to_remove = np.where(self.search_domain==X_af)[0]
+        self.search_domain=np.delete(self.search_domain, to_remove, axis=0)
+
+        X_batch = X_af
+        for j in range(1, self.batch_size):
+            AF = np.delete(AF, idx).reshape(-1,1)
+            LAW = self.objective.compute_value(AF, self.search_domain, X=X_batch) 
+            idx = np.argmax(LAW)
+            new_sample = np.atleast_2d(self.search_domain[idx])
+            to_remove = np.where(self.search_domain==new_sample)[0]
+            self.search_domain=np.delete(self.search_domain, to_remove, axis=0)
+            L_max = LAW[idx]
+
+            if verbose:
+                print("New sample: {}, max LAW:{}".format(new_sample, L_max))
+            X_batch = np.vstack([X_batch, new_sample])
+            if X_testing is not None:
+                L_obj_val.append(self.objective.compute_value(AF, self.search_domain, X=X_testing))
+        return X_batch, L_obj_val
+
+class LAW_BOptimizer(object):
+
+    def  __init__(
+                    self,
+                    acquisition_name,
+                    batch_size,
+                    law_params,
+                    kernel, 
+                    optimize_restarts,
+                    weigth_function =  None
+                    ):
+        self.batch_size = batch_size
+        self.acquisition_name = acquisition_name
+        self.law_params = law_params
+        self.kernel = kernel
+        self.optimize_restarts = optimize_restarts
+
+    def create_model(self, X, Y,  **constraints_dict):
+        ''' Creates model and sets ups self.model=model'''
+        kernel = self.kernel.copy()
+        noise_var = np.std(Y)*0.001
+
+        model = GPModel(kernel, optimize_restarts=self.optimize_restarts)
+        model.model = GPRegression(X, Y, kernel=kernel, noise_var=noise_var)
+        self.apply_model_constraints(model, constraints_dict)
+        model.model.optimize()
+        self.model=model
+
+    @staticmethod
+    def model_from_file():
+        ''' Builds model from loaded file '''
+        pass
+
+    def apply_model_constraints(self, constraints_dict):
+        '''  '''
+        if self.model is None:
+            raise  Exception("Error: optimizer has no model")
+        for k in constraints_dict.keys():
+            for j, c in constraints_dict[k].items():
+                if isinstance(c, float):
+                    self.model.model.parameters[int(k)].parameters[int(j)].constrain_fixed(c)
+                elif isinstance(c, list):
+                    self.model.model.parameters[int(k)].parameters[int(j)].constrain_bounded(*c)
+
+
+    def update_model(self, X_new, Y_new):
+
+        X= np.vstack([self.model.model.X, X_new ])
+        Y = np.vstack([self.model.model.Y, Y_new])
+        self.model.model.set_XY(X, Y)
+        self.model.model.optimize()
+
+
+
+    def create_evaluator(self, domain,  weight_func=None):
+        '''
+            Creates the evaluator and sets it as attribute of the LAW_BOptimizer 
+        '''
+        space = Design_space(domain)
+
+        ## -- Create Acquisition function --
+
+        if self.acquisition_name == "EI":
+            from GPyOpt.acquisitions.EI import AcquisitionEI as EI
+            AF = EI(self.model, space, optimizer=None)
+        else:
+            raise NotImplementedError("Only EI acquisition function implemented at the moment")
+        
+        ## -- Create the wirght function for LAW2ORDER --
+
+        v_lim = self.law_params['LAW_params']['var_size']
+        c = self.law_params['LAW_params']['c_value']
+        b = self.law_params['LAW_params']['b_value']
+        if weight_func is None:
+            weight_func = lambda x: c +b*x
+        LAW_func  = LAW_acq(self.model, weight_func, v_lim=v_lim, normalize_values=False)
+        X_domain  = domain['domain']
+        evaluator = LAW_Evaluator(self.batch_size, X_domain, AF, objective=LAW_func, verbose = True)
+        self.evaluator = evaluator 
+
+    def suggest_batch(self, X_new, Y_new,  **constraints_dict):
+
+        if self.model is None:
+            model = self.create_model( X_new, Y_new,  **constraints_dict)
+            self.model = model
+            X_all, Y_all  = X_new, Y_new
+        else:
+            X = self.model.model.X.copy()
+            Y = self.model.model.Y.copy()
+            X_all = np.vstack([X, X_new])
+            Y_all = np.vstack([Y, Y_new])
+        self.model.updateModel(X_all=X_all, Y_all=Y_all,
+                               X_new=None, Y_new=None
+                               )
+        X_batch =self.evaluator.compute_batch()[0]
+        return X_batch
+
+
+# %%
+if __name__ == "__main__":
+    from GPy.kern import RBF
+
+
+    law_params = {
+                "var_size":10,
+                "c_value": 1,
+                "b_value": 10
+                }
+    
+    model_constraints = {
+                        "0":{"0":0.5,"1":0.1}, 
+                        "1": {"0":[1e-6, 10]}
+                        }
+    GPy_kernel = RBF(input_dim=2, ARD=True)
+    kernel = Kernels.CoulombKernel(input_dim=2, GPy_kern=GPy_kernel, seed=123)
+
+
+    bopt = LAW_BOptimizer(
+                    acquisition_name="EI",
+                    batch_size=3,
+                    weigth_function=None,
+                    law_params=law_params,
+                    kernel=kernel,
+                    optimize_restarts=1
+                    )
+
+
+# %%
